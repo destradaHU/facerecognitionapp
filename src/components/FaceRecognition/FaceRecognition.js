fee941a04d092535be0f68c69c84ac17d7340839
destradaHU <destrada@harding.edu>
2020-04-20 18:14:08 -0600

    added the face box

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -6,7 +6,9 @@ const FaceRecognition = ({urlImage, box}) =>{
         <div className='center ma'>
             <div className='absolute mt2'>
                 <img id='inputimage' src={urlImage} alt='' width='500px' height='auto' />
-                <div className='bounding-box'></div>
+                <div 
+                className='bounding-box' 
+                style={{top: box.topRow,right:box.rightCol,bottom:box.bottomRow,left:box.leftCol}}></div>
             </div>
         </div>
     );
